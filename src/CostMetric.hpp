f701f9e9a1ad64a6fc9daa92c3e5f31bd7bf8543
César Eduardo Corrêa <cesarecorrea94@gmail.com>
2018-05-30 00:47:56 -0300

    Semi-Finalizando 'Node'
    Falta tratar o bloco 'else' no construtor, responsável pelos nodos folhas.

--- src/CostMetric.hpp
+++ src/CostMetric.hpp
@@ -8,33 +8,33 @@
 #ifndef COSTMETRIC_HPP_
 #define COSTMETRIC_HPP_
 
+typedef double costType;
+constexpr static costType cost_MAX = DBL_MAX;
 
 template<typename instanceType>
 class CostMetric {
 public:
+	virtual ~CostMetric() { }
 	virtual void passInstance(instanceType instance) = 0;
-	virtual double costFunction() = 0;
+	virtual costType costFunction() = 0;
 };
 
 template<typename instanceType, unsigned nInstances>
-class SDR : public CostMetric<instanceType> {//Standard Deviation Reduction
-	enum {esq, dir};
+class SDR : public CostMetric<instanceType> { // Standard Deviation Reduction
+	enum { esq, dir };
 	enum { sum_xi_squared, sum_xi };
 	double auxStructure[2][2];
 	unsigned countPasses;
 public:
-	SDR(instanceType ds[nInstances]){
-		this->countPasses = 0;
-		for(unsigned i; i<2; ++i) for(unsigned j; j<2; ++j)
-			this->auxStructure[i][j] = 0;
-		for(unsigned index = 0; index < nInstances; ++index){
+	SDR(instanceType ds[nInstances]) : auxStructure { 0 }, countPasses(0) {
+		for(unsigned index = 0; index < nInstances; ++index) {
 			instanceType &instance = ds[index];
 			int squared = instance.value() * instance.value();
 			this->auxStructure[dir][sum_xi_squared] += squared;
 			this->auxStructure[dir][sum_xi		]   += instance.value();
 		}
 	}
-	void passInstance(instanceType instance) {
+	void passInstance(instanceType instance) override {
 		int squared = instance.value() * instance.value();
 		this->auxStructure[esq][sum_xi_squared] += squared;
 		this->auxStructure[esq][sum_xi		]   += instance.value();
@@ -43,7 +43,7 @@ public:
 		// (xi - X)^2 = xi^2 - 2*xi*X + X^2
 		this->countPasses++;
 	}
-	double costFunction() {
+	costType costFunction() override {
 		unsigned count[] = { this->countPasses, nInstances-this->countPasses };
 		double retorno = 0;
 		for(unsigned pos = 0; pos < 2; ++pos) {
@@ -59,25 +59,22 @@ public:
 
 template<typename instanceType, unsigned nClasses, unsigned nInstances>
 class GiniImpurity : public CostMetric<instanceType> {
-	enum {esq, dir};
+	enum { esq, dir };
 	int auxStructure[2][nClasses];
 	unsigned countPasses;
 public:
-	GiniImpurity(instanceType ds[nInstances]){
-		this->countPasses = 0;
-		for(unsigned i; i<2; ++i) for(unsigned j; j<nClasses; ++j)
-			this->auxStructure[i][j] = 0;
-		for(unsigned index = 0; index < nInstances; ++index){
+	GiniImpurity(instanceType ds[nInstances]) : auxStructure { 0 }, countPasses(0) {
+		for(unsigned index = 0; index < nInstances; ++index) {
 			instanceType &instance = ds[index];
 			this->auxStructure[dir][instance.classe()]++;
 		}
 	}
-	void passInstance(instanceType instance) {
+	void passInstance(instanceType instance) override {
 		this->auxStructure[esq][instance.classe()]++;
 		this->auxStructure[dir][instance.classe()]--;
 		this->countPasses++;
 	}
-	double costFunction(){
+	costType costFunction() override {
 		return 0;
 	}
 };
--- src/Node.hpp
+++ src/Node.hpp
@@ -7,98 +7,143 @@
 
 #ifndef NODE_HPP_
 #define NODE_HPP_
-#include <limits.h> // INT_MAX
+#include <float.h> // DBL_MAX
 
 #include "CostMetric.hpp"
+#include "utils.hpp"
 
 template<typename instanceType>
-class Node {
-	Node<instanceType> *left, *right;
+class Node final {
 protected:
+	struct BaseCompare {
+		virtual ~BaseCompare() { }
+		virtual bool operator >=(instanceType instance) = 0;
+		virtual bool operator !=(instanceType instance) = 0;
+	};
 	template<typename attrType>
-	struct Compare {
+	struct Compare final : BaseCompare {
 		attrType value;
 		attrType (*getAttr)(instanceType);
-		virtual bool operator >=(instanceType instance) {
+		Compare(attrType (*g)(instanceType)) : getAttr(g) { }
+		bool operator >=(instanceType instance) override {
 			return this->value >= this->getAttr(instance);
 		}
-		virtual bool operator !=(instanceType instance) {
+		bool operator !=(instanceType instance) override {
 			return this->value != this->getAttr(instance);
 		}
+		void operator =(instanceType instance) {
+			this->value = this->getAttr(instance);
+		}
 	};
-	template<typename attrType>
-	struct Split {
-		attrType value;
-		double cost;
+	struct Split final {
+		unsigned leftCardinality;
+		costType cost;
+		BaseCompare *comparator;
 	};
+private:
+	Node<instanceType> *left, *right;
+	BaseCompare *comparator;
 public:
-	//Node() { }
-	//virtual ~Node() { }
-
-	template<unsigned nInstances, typename attrType>
-	inline Split<attrType> constructor(
-			instanceType ds[nInstances],
-			CostMetric<instanceType> &auxStruct,
-			attrType (*getAttr)(instanceType)
-	) {
-		//Split<attrType> sp = this->testSplit<nInstances>(ds, auxStruct, getAttr);
-		//return sp;
+	~Node() {
+		delete comparator;
+		delete left;
+		delete right;
 	}
 
-	template<unsigned nInstances, typename attrType, typename ...attrType_>
-	inline void* constructor(
-			instanceType ds[nInstances],
+	template<typename ...attrType_>
+	Node(	instanceType ds[],
+			unsigned cardinality,
+			CostMetric<instanceType> &auxStruct,
+			attrType_ (*...getAttr_)(instanceType));
+
+	template<typename attrType, typename ...attrType_>
+	Split testSplit(
+			instanceType ds[],
+			unsigned cardinality,
 			CostMetric<instanceType> &auxStruct,
 			attrType (*getAttr)(instanceType),
 			attrType_ (*...getAttr_)(instanceType));
 
-	template<unsigned nInstances, typename attrType>
-	Split<attrType> testSplit(
-			instanceType ds[nInstances],
+	template<typename attrType>
+	Split testSplit(
+			instanceType ds[],
+			unsigned cardinality,
 			CostMetric<instanceType> &auxStruct,
 			attrType (*getAttr)(instanceType));
 };
 
 template<typename instanceType>
-template<unsigned nInstances, typename attrType, typename ...attrType_>
-void* Node<instanceType>::constructor(
-		instanceType ds[nInstances],
+template<typename ...attrType_>
+Node<instanceType>::Node(
+		instanceType ds[],
+		unsigned cardinality,
+		CostMetric<instanceType> &auxStruct,
+		attrType_ (*...getAttr_)(instanceType)
+){
+	if(cardinality > 1) {
+		Split sp = this->testSplit<attrType_...>(ds, cardinality, auxStruct, getAttr_...);
+		this->comparator = sp.comparator;
+		this->left  = new Node<instanceType>( ds, 									sp.leftCardinality, auxStruct, getAttr_...);
+		this->right = new Node<instanceType>(&ds[sp.leftCardinality], cardinality - sp.leftCardinality, auxStruct, getAttr_...);
+	} else {
+		// ...
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+}
+
+template<typename instanceType>
+template<typename attrType, typename ...attrType_>
+typename Node<instanceType>::Split Node<instanceType>::testSplit(
+		instanceType ds[],
+		unsigned cardinality,
 		CostMetric<instanceType> &auxStruct,
 		attrType (*getAttr)(instanceType),
 		attrType_ (*...getAttr_)(instanceType)
 ) {
-	//Split<attrType> sp = this->testSplit<nInstances>(ds, auxStruct, getAttr);
-	//Split<attrType_...> sp_ = this->constructor<nInstances, attrType_...>(ds, auxStruct, getAttr_...);
-	//return sp.cost < sp_.cost ? sp : sp_;
+	Split sp = this->testSplit<attrType>(ds, cardinality, auxStruct, getAttr);
+	Split sp_pack = this->testSplit<attrType_...>(ds, cardinality, auxStruct, getAttr_...);
+	if(sp.cost < sp_pack.cost){
+		delete sp_pack.comparator;
+		return sp;
+	} else {
+		delete sp.comparator;
+		return sp_pack;
+	}
 }
 
 template<typename instanceType>
-template<unsigned nInstances, typename attrType>
-Node<instanceType>::Split<attrType> Node<instanceType>::testSplit(
-		instanceType ds[nInstances],
+template<typename attrType>
+typename Node<instanceType>::Split Node<instanceType>::testSplit(
+		instanceType ds[],
+		unsigned cardinality,
 		CostMetric<instanceType> &auxStruct,
 		attrType (*getAttr)(instanceType)
 ) {
-	quicksort(ds, nInstances, getAttr);
-	Split<attrType> split,
-	aux = { .value = getAttr(ds[0]),
-			.cost = INT_MAX };
-	for(unsigned index = 0; index < nInstances; index++) {
-		instanceType &instance = ds[index];
+	quicksort(ds, cardinality, getAttr);
+	Compare<attrType> auxCompare(getAttr), compare(getAttr);
+	costType auxCost = cost_MAX, cost;
+	unsigned index;
+	auxCompare = ds[0];
+	for(unsigned auxIndex = 0; auxIndex < cardinality; auxIndex++) {
+		instanceType &instance = ds[auxIndex];
 		// Procura-se o próximo valor que será o divisor do dataset
 		// enquanto o valor não muda, vai passando os elementos para o conjunto esquerdo
-		if(aux.value != getAttr(instance)) {		// se o valor mudar
-			aux.value = getAttr(instance);			// tem-se um novo divisor do dataset
-			aux.cost = auxStruct.costFunction();	// calcula-se o custo
-			if(aux.cost < split.cost) {				// se o custo for menor
-				split = aux;						// guarda o novo valor de divisão
+		if(auxCompare != instance) {			// se o valor mudar
+			auxCompare = instance;				// tem-se um novo divisor do dataset
+			auxCost = auxStruct.costFunction();	// calcula-se o custo
+			if(auxCost < cost) {				// se o custo for menor
+				cost = auxCost;					// guarda o novo valor de divisão
+				compare = auxCompare;
+				index = auxIndex;
 			}
 			// a partir de agora, 'instance' fará parte do conjunto esquerdo
 		}
 		// move o elemento para o conjunto esquerdo
 		auxStruct.passInstance(instance);
 	}
-	return split;
+	Split retorno = { .leftCardinality = index, .cost = cost, .comparator = new Compare<attrType>(compare) };
+	return retorno;
 }
 
 #endif /* NODE_HPP_ */
