f02ce28041ceb7c63611f559988a89516ff5982b
César Eduardo Corrêa <cesarecorrea94@gmail.com>
2018-06-18 17:23:26 -0300

    Tratamento de erros de overflow
    * Adição de alguns 'specifiers' (cons, override)
    * Renomeação de alguns atributos (agora começam com '_')
    * Criação de métodos auxiliares (average, SD, CV)
    * Mudança no método stop para trabalhar com CV como critério de parada
    * Alteração do tipo de variáveis na classe SDR para evitar overflow (i.e. quadrado do valor da instância)

--- src/CostMetric.hpp
+++ src/CostMetric.hpp
@@ -8,6 +8,7 @@
 #ifndef COSTMETRIC_HPP_
 #define COSTMETRIC_HPP_
 
+#include <cassert>
 typedef double costType;
 constexpr static costType cost_MAX = DBL_MAX;
 
@@ -15,55 +16,62 @@ template<typename instanceType, typename predictType>
 class CostMetric {
 public:
 	virtual ~CostMetric() { }
-	virtual void passInstance(instanceType instance) = 0;
-	virtual costType costFunction() = 0;
-	virtual bool stop(predictType &value) = 0;
+	virtual void passInstance(const instanceType &instance) = 0;
+	virtual costType costFunction() const = 0;
+	virtual bool stop(predictType &value) const = 0;
 };
 
 template<typename instanceType, typename predictType>
 class SDR : public CostMetric<instanceType, predictType> { // Standard Deviation Reduction
-	enum { esq, dir };
+	enum position { esq, dir };
 	enum { sum_xi_squared, sum_xi };
-	unsigned cardinality;
-	unsigned countPasses;
-	double auxStructure[2][2];
+	unsigned _cardinality[2];
+	typedef long double auxType;
+	auxType _auxStructure[2][2];
 public:
 	SDR(instanceType ds[], unsigned nInstances)
-: cardinality(nInstances), countPasses(0), auxStructure { 0 } {
+: _cardinality { [esq]=0, [dir]=nInstances }, _auxStructure { {0} } {
 		for(unsigned index = 0; index < nInstances; ++index) {
 			instanceType &instance = ds[index];
-			int squared = instance.value() * instance.value();
-			this->auxStructure[dir][sum_xi_squared] += squared;
-			this->auxStructure[dir][sum_xi		]   += instance.value();
+			auxType value = instance.value();
+			this->_auxStructure[dir][sum_xi] += value;
+			value *= value;
+			this->_auxStructure[dir][sum_xi_squared] += value;
 		}
 	}
-	void passInstance(instanceType instance) override {
-		int squared = instance.value() * instance.value();
-		this->auxStructure[esq][sum_xi_squared] += squared;
-		this->auxStructure[esq][sum_xi		]   += instance.value();
-		this->auxStructure[dir][sum_xi_squared] -= squared;
-		this->auxStructure[dir][sum_xi		]   -= instance.value();
+	void passInstance(const instanceType &instance) override {
+		this->_cardinality[esq]++;
+		this->_cardinality[dir]--;
 		// (xi - X)^2 = xi^2 - 2*xi*X + X^2
-		this->countPasses++;
+		auxType value = instance.value();
+		this->_auxStructure[esq][sum_xi] += value;
+		this->_auxStructure[dir][sum_xi] -= value;
+		value *= value;
+		this->_auxStructure[esq][sum_xi_squared] += value;
+		this->_auxStructure[dir][sum_xi_squared] -= value;
 	}
-	costType costFunction() override {
-		unsigned count[] = { this->countPasses, this->cardinality-this->countPasses };
-		double retorno = 0;
-		for(unsigned pos = 0; pos < 2; ++pos) {
-			double average = auxStructure[pos][sum_xi] / count[pos];
-			retorno +=	(auxStructure[pos][sum_xi_squared] +
-						-2 * auxStructure[pos][sum_xi] * average +
-						this->countPasses * (average * average) ) *
-						count[pos] / this->cardinality;
-		}
-		return retorno;
+	costType costFunction() const override {
+		unsigned total = this->_cardinality[esq] + this->_cardinality[dir];
+		return (this->SD(esq) * this->_cardinality[esq] +
+				this->SD(dir) * this->_cardinality[dir] ) / total;
 	}
-	bool stop(predictType &value) override {
-		if(this->cardinality == 1) {
-			value = this->auxStructure[dir][sum_xi] / this->cardinality;
-			return true;
-		}
-		return false;
+	bool stop(predictType &value) const override {
+		assert(this->_cardinality[esq] == 0);
+		assert(this->_cardinality[dir] >= 1);
+		value = this->average(dir);
+		return (this->_cardinality[dir] == 1 || this->CV(dir) < 0.1);
+	}
+	inline auxType average(position pos) const {
+		return this->_auxStructure[pos][sum_xi] / this->_cardinality[pos];
+	}
+	inline auxType SD(position pos) const { // Standard Deviation
+		auxType average = this->average(pos);
+		return	this->_auxStructure[pos][sum_xi_squared] +
+				-2 * this->_auxStructure[pos][sum_xi] * average +
+				this->_cardinality[pos] * (average * average);
+	}
+	inline auxType CV(position pos) const { // Coefficiet of Variation
+		return this->SD(pos) / this->average(pos);
 	}
 };
 
@@ -81,15 +89,15 @@ public:
 			this->auxStructure[dir][instance.classe()]++;
 		}
 	}
-	void passInstance(instanceType instance) override {
+	void passInstance(const instanceType &instance) override {
 		this->auxStructure[esq][instance.classe()]++;
 		this->auxStructure[dir][instance.classe()]--;
 		this->countPasses++;
 	}
-	costType costFunction() override {
+	costType costFunction() const override {
 		return 0;
 	}
-	bool stop(predictType &value) override {
+	bool stop(predictType &value) const override {
 		if(this->cardinality == 1) {
 			// value = ...
 			return true;
